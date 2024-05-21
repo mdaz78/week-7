c861b52a7fdf6ae94d97975486c2102123db38dc
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-21 15:39:00 +0530

    route using useNavigate hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <Link to="/">Landing</Link>
-      <Link to="/dashboard">Dashboard</Link>
+      {/* <Link to="/">Landing</Link>
+      <Link to="/dashboard">Dashboard</Link> */}
+
+      <button onClick={() => navigate("/")}>Landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
     </div>
   );
 };
